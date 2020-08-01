62fa5fe59d93d951dc4301f4e5f24ed41754ccc0
Utsav Kumar <utsavkumar9920@gmail.com>
2020-08-01 12:37:59 +0530

    Final Commi

---
+++ config/config.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.twilio = {
+    authToken: 'Show',
+    accountSid: 'AC95cc07e98b7e37aecc045250a8e7b1a6'
+};
+
+config.mlab = {
+    username: 'utsavkumar9920@gmail.com',
+    password: '99201055'
+};
+
+config.port = 3000;
+config.address = "Noida, Uttar Pradesh 201309";
+
+module.exports = config;
